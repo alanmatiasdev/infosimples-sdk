452e8ef8ffc95f1ef6c041dffccb61d1651ac770
Alan Matias <alanmatias@almatsoft.com>
2026-08-26 21:31:24 -0300

    feat(anatel): add celularLegal endpoint
    Adds the Anatel category (celular-legal / IMEI lookup), previously
    unimplemented, from the InfoSimples OpenAPI spec.

--- lib/resources/index.ts
+++ lib/resources/index.ts
@@ -4,6 +4,7 @@ import caixa from './caixa';
 import portalTransparencia from './portalTransparencia';
 import sit from './sit';
 import ofac from './ofac';
+import anatel from './anatel';
 
 export * from './receitaFederalTypes';
 export * from './tribunaisTypes';
@@ -16,4 +17,5 @@ export default {
   portalTransparencia,
   sit,
   ofac,
+  anatel,
 };
